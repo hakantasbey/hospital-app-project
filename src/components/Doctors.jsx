a0882872ed74fc790450ffc0c877d011d44ff082
hakan <tasbeyhakan@gmail.com>
2023-12-17 23:37:15 +0300

    second commit

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { doctorData } from "../helper/data";
+import CardDoctor from "./CardDoctor";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Doctors = () => {
   return (
-    <div>Doctors</div>
-  )
-}
+    <Container>
+      <h3>Our Doctors</h3>
+      <Row className="g-4">
+        {doctorData.map((data) => (
+          <Col key={data.id} >
+            <CardDoctor data={data} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
-export default Doctors
+export default Doctors;
